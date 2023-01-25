8f2ba0918a19da16849dd42bf95c87d9fc69d406
rodneyems <rodney.martins@live.com>
2023-01-25 19:55:17 -0300

    add text

--- section2-Recursion/2 - Factorial.py
+++ section2-Recursion/2 - Factorial.py
@@ -1,3 +1,16 @@
+# factorial
+# Write a function factorial which accepts a number and returns the factorial of that number. A factorial 
+# is the product of an integer and all the integers below it; e.g., factorial four ( 4! ) is equal 
+# to 24, because 4 * 3 * 2 * 1 equals 24. factorial zero (0!) is always 1.
+
+# Examples
+
+# factorial(1) # 1
+# factorial(2) # 2
+# factorial(4) # 24
+# factorial(7) # 5040
+
+
 import sys
 sys.setrecursionlimit(10000)
 
--- section2-Recursion/3 - Fibonacci.py
+++ section2-Recursion/3 - Fibonacci.py
@@ -1,3 +1,14 @@
+# fib
+# Write a recursive function called fib which accepts a number and returns the nth number in the Fibonacci sequence. 
+# Recall that the Fibonacci sequence is the sequence of whole numbers 0, 1, 1, 2, 3, 5, 8, ... which starts with 0 and 1, and where every number 
+# thereafter is equal to the sum of the previous two numbers.
+
+# Examples
+
+# fib(4) # 3
+# fib(10) # 55
+# fib(28) # 317811
+# fib(35) # 9227465
 def fibonacci(n):
     assert n >=0 and int(n) == n , 'Fibonacci number cannot be negative number or non integer.'
     if n in [0,1]:
